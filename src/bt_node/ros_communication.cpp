34e63bf9811df9b87f615a72a06f9614d18ec79f
MaslinuPoimal <mykyta.denysov@gmail.com>
2019-01-26 00:08:43 +0100

    Adjust remappings

--- src/bt_node/ros_communication.cpp
+++ src/bt_node/ros_communication.cpp
@@ -96,7 +96,7 @@ void setup_ros_communication(ros::NodeHandle *nh) {
 
     /* Topic Subscribers Setup */
     environment_model_subscriber = nh->subscribe("env_in", 4, &EnvModel::subscriber_callback);
-    car_data_subscriber = nh->subscribe("drive_state_out", 4, &car_data_callback);
+    car_data_subscriber = nh->subscribe("drive_state_in", 4, &car_data_callback);
 
     /* Topic Publishers Setup */
     trajectory_publisher = nh->advertise<drive_ros_msgs::TrajectoryMetaInput>("trajectory_metadata", 64);
