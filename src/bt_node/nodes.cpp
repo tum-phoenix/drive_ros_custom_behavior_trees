d20a1d230ee0706c5c49d340da2d433b25e116c2
Philip Höbler <philip@hoebler.com>
2019-02-02 15:47:09 -0800

    temp

--- src/bt_node/nodes.cpp
+++ src/bt_node/nodes.cpp
@@ -314,19 +314,17 @@ namespace NODES {
             start_waiting = true;
         }
         else {
-            if((EnvModel::object_min_lane_distance(drive_ros_msgs::Lane::LEFT) < oncoming_traffic_clearance 
+            /*if(EnvModel::object_min_lane_distance(drive_ros_msgs::Lane::LEFT) < oncoming_traffic_clearance 
                 || (EnvModel::barred_area_left_distance() == -1 ? 10000 : EnvModel::barred_area_left_distance()) < oncoming_traffic_clearance
-                || (EnvModel::pass_by_on_right_distance() == -1 ? 10000 : EnvModel::pass_by_on_right_distance()) < oncoming_traffic_clearance)
-                && (!start_waiting 
-                    && (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - waiting_started).count() > 2000))) { //Abort, there's no room to overtake.
+                || (EnvModel::pass_by_on_right_distance() == -1 ? 10000 : EnvModel::pass_by_on_right_distance()) < oncoming_traffic_clearance) { //Abort, there's no room to overtake.
                     set_state(SUCCESS); //Go to "switch to right lane" and maybe then go back to "switch to left lane" again to try once more.
             }
-            else {
+            else {*/
                 drive_ros_msgs::TrajectoryMetaInput *msg = new drive_ros_msgs::TrajectoryMetaInput();
                 msg->control_metadata = drive_ros_msgs::TrajectoryMetaInput::STANDARD;
                 msg->max_speed = general_max_speed; //Spend as little time as possible on left lane
                 msg_handler.addMessageSuggestion(msg);
-            }
+            /*}*/
         }
     }
 
