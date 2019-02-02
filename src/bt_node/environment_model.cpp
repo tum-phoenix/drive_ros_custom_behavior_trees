f21ceae3e6b1f9268a80ef1461d54cd943805063
Philip Höbler <philip@hoebler.com>
2019-02-02 05:20:01 -0800

    Added debug output

--- src/bt_node/environment_model.cpp
+++ src/bt_node/environment_model.cpp
@@ -214,6 +214,9 @@ namespace EnvModel {
 
     bool start_box_was_closed = false;
     bool start_box_open() {
+        ROS_INFO_STREAM("Already detected startbox: " << start_box_was_closed << ", d=" << fmin(
+                fmin(object_min_lane_distance(drive_ros_msgs::Lane::LEFT), object_min_lane_distance(drive_ros_msgs::Lane::RIGHT)), 
+                fmin(object_min_lane_distance(drive_ros_msgs::Lane::LEFT_SIDE), object_min_lane_distance(drive_ros_msgs::Lane::RIGHT_SIDE))));
         if(!start_box_was_closed && 
             (fmin(
                 fmin(object_min_lane_distance(drive_ros_msgs::Lane::LEFT), object_min_lane_distance(drive_ros_msgs::Lane::RIGHT)), 
@@ -222,7 +225,9 @@ namespace EnvModel {
             start_box_was_closed = true;
             ROS_INFO_STREAM("Start box detected");
         }
-        return start_box_was_closed && fmin(object_min_lane_distance(drive_ros_msgs::Lane::LEFT), object_min_lane_distance(drive_ros_msgs::Lane::RIGHT)) > max_start_box_distance;
+        return start_box_was_closed && fmin(
+                fmin(object_min_lane_distance(drive_ros_msgs::Lane::LEFT), object_min_lane_distance(drive_ros_msgs::Lane::RIGHT)), 
+                fmin(object_min_lane_distance(drive_ros_msgs::Lane::LEFT_SIDE), object_min_lane_distance(drive_ros_msgs::Lane::RIGHT_SIDE))) > max_start_box_distance;
     }
 
     bool object_on_lane(int lane) {
